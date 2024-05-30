595ed68799a7762ef05d7a1a4c160d950c0fc595
Tetyana <tetyana.chouteau@gmail.com>
2024-05-30 21:26:25 +0200

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
 import styles from "./Header.module.css"
 import Navigation from './Navigation';
 import Logo from '../assets/images/logo.png'
+import background from '../assets/images/headerimg.png'
+
 function Header() {
 
     return (
-        <header className={styles.header}>
+        <header className={styles.header} style={{backgroundImage: `url(${background})`}}>
             <img src={Logo} alt="Logo HRnet"></img>
-            <div class="title">
-                <h1>HRnet</h1>
-            </div>
             <Navigation />
         </header>
     );
