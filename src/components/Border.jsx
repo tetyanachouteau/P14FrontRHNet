9cae97aee9509635b2f1cb29b313c4659a3361e3
Tetyana <tetyana.chouteau@gmail.com>
2024-05-30 21:22:07 +0200

    Ajout Composant Border: backgroundImage: bouquet pour pages Error et Home uniquement, pas pour list

---
+++ src/components/Border.jsx
@@ -0,0 +1,15 @@
+import styles from "./Border.module.css"
+import bouquet from '../assets/images/bouquet.png'
+
+function Border() {
+
+    return (
+        <div className={styles.border} style={{ backgroundImage: `url(${bouquet})` }}></div>
+    );
+}
+
+
+export default Border;
+
+
+
