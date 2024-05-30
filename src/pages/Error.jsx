6c1912565fb173e6676d1d9ed5749a7a462f19e3
Tetyana <tetyana.chouteau@gmail.com>
2024-05-30 21:27:58 +0200

    Update Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,16 @@
 import styles from "./Error.module.css"
 import React from "react";
-import Error404 from "../assets/images/404.png"
+import Border from "../components/Border";
 
 function Erreur() {
   // Composant pour afficher une page d'erreur 404
   return (
-    <div className={styles.erreur}>
-      <img src={Error404} alt="Error 404" />
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+    <div className={styles.content}>
+      <Border/>
+      <div className={styles.erreur}>
+        <p className={styles.big}>404</p>
+        <p>Oops! The page you are requesting does not exist.</p>
+      </div>
     </div>
   );
 }
