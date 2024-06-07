4aaeaf429e4420a64b67733509f788cf0362ed9d
Tetyana <tetyana.chouteau@gmail.com>
2024-06-07 15:23:15 +0200

    Homme css et import des composant Input, Select...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,86 +2,151 @@
 
 import React, { useState } from 'react';
 import styles from './Home.module.css';
-import { Button, Form } from 'react-bootstrap';
-import Modale from '../components/Modale';
+import Modal from '../components/Modal';
 import Border from '../components/Border';
+import Input from '../components/Input';
+import Select from '../components/Select';
+import GroupInput from '../components/GroupInput';
+import Button from '../components/Button';
 
 function Formulaire({ data }) {
     const [isModalVisible, setModalVisible] = useState(false);
 
+    const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
+    const onChangeFirstName = (event) => {
+        const value = event.target.value;
+        setFirstName(value);
+        setFirstNameError("");
+    }
+
+    const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const onChangeLastName = (event) => {
+        const value = event.target.value;
+        setLastName(value);
+        setLastNameError("");
+    }
+
+    const [dateOfBirth, setDateOfBirth] = useState("");
+    const [dateOfBirthError, setDateOfBirthError] = useState("");
+    const onChangeDateOfBirth = (event) => {
+        const value = event.target.value;
+        setDateOfBirth(value);
+        setDateOfBirthError("");
+    }
+
+    const [startDate, setStartDate] = useState("");
+    const [startDateError, setStartDateError] = useState("");
+    const onChangeStartDate = (event) => {
+        const value = event.target.value;
+        setStartDate(value);
+        setStartDateError("");
+    }
+
+    // const [firstName, setFirstName] = useState();
+    // const [firstNameError, setFirstNameError] = useState();
+    // const [firstName, setFirstName] = useState();
+    // const [firstNameError, setFirstNameError] = useState();
+    // const [firstName, setFirstName] = useState();
+    // const [firstNameError, setFirstNameError] = useState();
+    // const [firstName, setFirstName] = useState();
+    // const [firstNameError, setFirstNameError] = useState();
+    // const [firstName, setFirstName] = useState();
+    // const [firstNameError, setFirstNameError] = useState();
+
+
     const saveEmployee = () => {
+        if (!firstName) {
+            setFirstNameError("First name is mandatory")
+            return;
+        }
+        if (!lastName) {
+            setLastNameError("Last name is mandatory")
+            return;
+        }
+        if (!dateOfBirth){
+            setDateOfBirthError("Date of birth is mandatory")
+            return;
+        }
+        console.log(dateOfBirth)
+        if (new Date(dateOfBirth) > new Date()){
+            setDateOfBirthError("Date of birth should be before today")
+            return;
+        }
+        if (!startDate){
+            setStartDateError("Start date is mandatory")
+            return;
+        }
+
         // Logique de sauvegarde des employés (à implémenter)
         setModalVisible(true);
+        setFirstName("");
+        setLastName("");
+        setDateOfBirth("");
+        setStartDate("");
         setTimeout(() => {
             setModalVisible(false);
         }, 3000); // Cache la modal après 3 secondes
     };
 
-    const closeModale = () => {
+    const closeModal = () => {
         setModalVisible(false);
+    }
+    //esxemple: X ferme modal
+    //comment je fais json de config pour qu'il soit bien simple facile et perment de configuer facillement, doc , packege
+    const customConfig = {
+        buttons: [
+            {
+                label: 'OK',
+                className: styles.button,
+                action: closeModal
+            },
+            // {
+            //     label: 'Cancel',
+            //     className: styles.button,
+            //     action: closeModal
+            // }
+        ],
+        //title: "Information",
+        //showCloseIcon: true,
+        onCloseIcon: closeModal
     }
 
     return (
         <div className={styles.content}>
-            <Border/>
+            <Border />
             <div className={styles.home}>
-                <h2>Create Employee</h2>
-                <Form>
-                    <Form.Group className="mb-3" controlId="first-name">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="last-name">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Last Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="date-of-birth">
-                        <Form.Label>Date of Birth</Form.Label>
-                        <Form.Control type="date" placeholder="Date of Birth" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="start-date">
-                        <Form.Label>Start Date</Form.Label>
-                        <Form.Control type="text" placeholder="Start Date" />
-                    </Form.Group>
-                    <fieldset className={styles.address}>
-                        <legend>Address</legend>
-                        <Form.Group className="mb-3" controlId="street">
-                            <Form.Label>Street</Form.Label>
-                            <Form.Control type="text" placeholder="Street" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="city">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control type="text" placeholder="City" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="state">
-                            <Form.Label>State</Form.Label>
-                            <Form.Control type="text" placeholder="State" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="zip-code">
-                            <Form.Label>Zip Code</Form.Label>
-                            <Form.Control type="number" placeholder="Zip Code" />
-                        </Form.Group>
-
-                    </fieldset>
-
-                    <Form.Group className="mb-3" controlId="department">
-                        <Form.Label>Department</Form.Label>
-                        <Form.Select aria-label="Department">
-                            <option>Sales</option>
-                            <option>Marketing</option>
-                            <option>Engineering</option>
-                            <option>Human Resources</option>
-                            <option>Legal</option>
-                        </Form.Select>
-                    </Form.Group>
+                <h2 className={styles.h2}>Create Employee</h2>
+                <form className={styles.form}>
+                    <Input controlId="first-name" label="First Name" type="text" placeholder="First Name" onChange={onChangeFirstName} hasError={firstNameError} value={firstName} />
+                    <Input controlId="last-name" label="Last Name" type="text" placeholder="Last Name" onChange={onChangeLastName} hasError={lastNameError} value={lastName} />
+                    <Input controlId="date-of-birth" label="Date of Birth" type="date" placeholder="Date of Birth" onChange={onChangeDateOfBirth} hasError={dateOfBirthError} value={dateOfBirth}  />
+                    <Input controlId="start-date" label="Start Date" type="date" placeholder="Start Date" onChange={onChangeStartDate} hasError={startDateError} value={startDate} />
+                    <GroupInput title="Address" className={styles.address}>
+                        <Input controlId="street" label="Street" type="text" placeholder="Street" />
+                        <Input controlId="city" label="City" type="text" placeholder="City" />
+                        <Input controlId="state" label="State" type="text" placeholder="State" />
+                        <Input controlId="zip-code" label="Zip Code" type="number" placeholder="Zip Code" />
+                    </GroupInput>
+                    <Select controlId="department" label="Department">
+                        <option>Sales</option>
+                        <option>Marketing</option>
+                        <option>Engineering</option>
+                        <option>Human Resources</option>
+                        <option>Legal</option>
+                    </Select>
                     <Button className={styles.button} variant="primary" onClick={saveEmployee}>Create</Button>
-                    <Modale show={isModalVisible} onClose={closeModale}>
+                    <Modal show={isModalVisible} config={customConfig}>
                         <p>Employee Created!</p>
-                    </Modale>
-                </Form>
+                    </Modal>
+                </form>
             </div>
         </div>
     );
 }
 
 export default Formulaire;
+
+//customiser: adapter, tout changer, couleur.. et ? icone de fermeture ou pas?
+//composant package avec plus tard
