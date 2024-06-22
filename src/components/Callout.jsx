46b68c533e9830d2c446da73ebb2e992675c87a1
Tetyana <tetyana.chouteau@gmail.com>
2024-06-22 22:25:37 +0200

    Add 3 callouts of erreur page

---
+++ src/components/Callout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Callout.module.css';
+
+/**
+ * Callout component
+ * @param {string} className - Additional CSS class names to apply to the callout.
+ * @param {string} type - Type of callout (e.g., 'success', 'warning', 'info').
+ * @param {string} title - Title of the callout.
+ * @param {string} icon - Icon to display in the callout.
+ * @param {ReactNode} children - The content of the callout.
+ */
+function Callout({ className, type = "erreur404", icon, title, children }) {
+    return (
+        <div className={`${styles.callout} ${styles[type]} ${className ?? ""}`}>
+            {icon && <div className={styles.icon}>{icon}</div>}
+            {title && <h3 className={styles.title}>{title}</h3>}
+            <div className={styles.content}>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+Callout.propTypes = {
+    className: PropTypes.string,
+    type: PropTypes.string,
+    title: PropTypes.string,
+    icon: PropTypes.node,
+    children: PropTypes.node.isRequired
+};
+
+export default Callout;
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,18 +1,42 @@
-import styles from "./Error.module.css"
+import styles from "./Error.module.css";
 import React from "react";
 import Border from "../components/Border";
+import Callout from "../components/Callout";
 
-function Erreur() {
-  // Composant pour afficher une page d'erreur 404
+const Erreur = () => {
+
   return (
     <div className={styles.content}>
-      <Border/>
-      <div className={styles.erreur}>
-        <p className={styles.big}>404</p>
-        <p>Oops! The page you are requesting does not exist.</p>
+      <Border />
+      <div>
+        😉
+        <Callout
+          title={"Error 404"}
+        >
+          &#9888; Oops! The page you are requesting does not exist.
+        </Callout>
+
+      </div>
+      <div>
+      🤩
+        <Callout
+          title={"Success"}
+          type={"success"}
+
+        >
+          &#9880; The operation was completed successfully!
+        </Callout>
+</div><div>😽
+        <Callout
+          title={"Help"}
+          type={"info"}
+        >
+           &#8520; Back to home page by clicking the nav link
+        </Callout>
       </div>
     </div>
+    
   );
-}
+};
 
 export default Erreur;
