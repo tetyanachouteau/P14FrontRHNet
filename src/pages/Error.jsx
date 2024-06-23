e2f55b2864ad071bf6414ac081a3d71b6bf00a41
Tetyana <tetyana.chouteau@gmail.com>
2024-06-23 12:30:59 +0200

    Add Callouts to Erreur, css responsif

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -8,35 +8,57 @@ const Erreur = () => {
   return (
     <div className={styles.content}>
       <Border />
-      <div>
-        😉
-        <Callout
-          title={"Error 404"}
-        >
-          &#9888; Oops! The page you are requesting does not exist.
-        </Callout>
+      <div className={styles.right}>
+        <div className={styles.callout}>
+          😉
+          <Callout
+            title={"Error 404"}
+          >
+            &#9888; Oops! The page you are requesting does not exist.
+          </Callout>
+        </div>
 
-      </div>
-      <div>
-      🤩
-        <Callout
-          title={"Success"}
-          type={"success"}
+        <div className={styles.callout}>
+          😽
+          <Callout
+            title={"Help"}
+            type={"info"}
+          >
+            &#8520; Back to home page by clicking the nav link.
+          </Callout>
+        </div>
 
-        >
-          &#9880; The operation was completed successfully!
-        </Callout>
-</div><div>😽
-        <Callout
-          title={"Help"}
-          type={"info"}
-        >
-           &#8520; Back to home page by clicking the nav link
-        </Callout>
+        <div className={styles.callout}>
+          💌
+          <Callout
+            title={"About HRnet - Wealth Health"}
+            type={"about"}
+          >
+            <div>
+              <p>&#9752; We are a technology department of the large financial company - WealthHealth, specializing in the finances of the flower market.</p>
+              <p>The company uses an internal web application called HRnet, which manages employee records.</p>
+              <p className={styles.p}>
+                Flower market: The flower and plant market represented a total amount of approximately €1.6 billion in 2022. In 2022, the 13,200 French florists captured 46% of sales in value.
+              </p>
+
+            </div>
+          </Callout>
+        </div>
       </div>
     </div>
-    
   );
 };
 
 export default Erreur;
+
+/*
+<div>
+  🤩
+  <Callout
+    title={"Success"}
+    type={"success"}
+  >
+    &#9880; The operation was completed successfully!
+  </Callout>
+</div>
+ */
