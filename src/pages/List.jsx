d617f685cac1e866fea0aa170e6dc683578ea994
Tetyana <tetyana.chouteau@gmail.com>
2024-07-01 17:37:26 +0200

    Add css at list search, show

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -7,8 +7,6 @@ function List() {
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [currentPage, setCurrentPage] = useState(1);
 
-    //const [stateRow, setStateRow] = useState(new Array());
-
     const columns = [
         'First Name',
         'Last Name',
@@ -79,7 +77,7 @@ function List() {
         }
     ];
 
-    const filteredData = data.filter(item => 
+    const filteredData = data.filter(item =>
         item.firstName.toLowerCase().includes(filterText.toLowerCase()) ||
         item.lastName.toLowerCase().includes(filterText.toLowerCase()) ||
         item.startDate.toLowerCase().includes(filterText.toLowerCase()) ||
@@ -103,67 +101,68 @@ function List() {
     const totalPages = Math.ceil(filteredData.length / rowsPerPage);
 
     return (
-        <div className={styles.home}>
-            <h1 className={styles.title}>Current Employees</h1>
-            <div className={`${styles.controls} ${styles.flex}`}>
-                <label>
-                    Show
-                    <select 
-                        value={rowsPerPage} 
-                        onChange={handlePerPageChange}
-                    >
-                        <option value={10}>10</option>
-                        <option value={25}>25</option>
-                        <option value={50}>50</option>
-                        <option value={100}>100</option>
-                    </select>
-                    entries
-                </label>
-                <label className={styles.label}>
-                    Search
-                    <input 
-                        type="text" 
-                        placeholder="Search1" 
-                        value={filterText} 
-                        onChange={e => setFilterText(e.target.value)}
-                        className={styles.search}
-                    />
-                </label>
-            </div>
-            <table className={`${styles.dataTable} ${styles.redText}`}>
-                <thead>
-                    <tr>
-                        {columns.map((column, index) => (
-                            <th key={index} className={styles.tableHeader}>{column}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage).map((row, index) => (
-                        <tr key={index} className={styles.tableRow}>
-                            {Object.keys(row).map((cell, indexCell) => (
-                                <td key={indexCell} className={styles.tableCell}>{row[cell]}</td>
+        <div className={styles.content}>
+            <div className={styles.home}>
+                <h1 className={styles.h1}>Current Employees</h1>
+                <div className={styles.controls}>
+                    <label className={styles.label}>
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            value={filterText}
+                            onChange={e => setFilterText(e.target.value)}
+                            className={styles.search}
+                        />
+                    </label>
+                    <label className={styles['entries-label']}>
+                        <h3>Show</h3>
+                        <select
+                            value={rowsPerPage}
+                            onChange={handlePerPageChange}
+                        >
+                            <option value={10}>10</option>
+                            <option value={25}>25</option>
+                            <option value={50}>50</option>
+                            <option value={100}>100</option>
+                        </select>
+                        <h3>entries</h3>
+                    </label>
+                </div>
+                <table className={styles.dataTable}>
+                    <thead>
+                        <tr>
+                            {columns.map((column, index) => (
+                                <th key={index} className={styles.tableHeader}>{column}</th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className={styles.pagination}>
-                <button 
-                    onClick={() => handlePageChange(currentPage - 1)} 
-                    disabled={currentPage === 1}
-                >
-                    Previous
-                </button>
-                <span>{`Page ${currentPage} of ${totalPages}`}</span>
-                <button 
-                    onClick={() => handlePageChange(currentPage + 1)} 
-                    disabled={currentPage === totalPages}
-                >
-                    Next
-                </button>
+                    </thead>
+                    <tbody>
+                        {filteredData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage).map((row, index) => (
+                            <tr key={index} className={styles.tableRow}>
+                                {Object.keys(row).map((cell, indexCell) => (
+                                    <td key={indexCell} className={styles.tableCell}>{row[cell]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className={styles.pagination}>
+                    <button
+                        onClick={() => handlePageChange(currentPage - 1)}
+                        disabled={currentPage === 1}
+                    >
+                        Previous
+                    </button>
+                    <span>{`Page ${currentPage} of ${totalPages}`}</span>
+                    <button
+                        onClick={() => handlePageChange(currentPage + 1)}
+                        disabled={currentPage === totalPages}
+                    >
+                        Next
+                    </button>
+                </div>
+                <Link to="/" className={styles.homeLink}>Home</Link>
             </div>
-            <Link to="/" className={styles.homeLink}>Home</Link>
         </div>
     );
 }
