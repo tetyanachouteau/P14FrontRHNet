cbb29a3cefe6656820a9397dbe923d01d3a66b7c
Tetyana <tetyana.chouteau@gmail.com>
2024-08-23 15:24:43 +0200

    Add 3.png at List title filters, update via UseState at  //99, Update List.css

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -5,6 +5,10 @@ import styles from './List.module.css';
 import Button from '../components/Button';
 import Callout from '../components/Callout';
 import API from '../services/API';
+import bothImage from '../assets/images/sort_both.png';
+import ascImage from '../assets/images/sort_asc.png';
+import descImage from '../assets/images/sort_desc.png';
+//import UseState
 
 function List() {
     // États pour la gestion des données du formulaire et de la pagination
@@ -25,9 +29,14 @@ function List() {
         'Department',
     ];
 
+    const defaultSort = columns.map(el => { return { "columnName": el, "sort": bothImage }; })
+    const [sortColumn, setSortColumn] = useState(defaultSort);
+
     // États pour les données filtrées et le nombre total de pages
     const [filteredData, setFilteredData] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
+
+    const [filteredUnsortedData, setFilteredUnsortedData] = useState([]);
 
     // useEffect pour récupérer les données des employés et les filtrer
     useEffect(() => {
@@ -35,8 +44,7 @@ function List() {
             // Appel à l'API pour récupérer les employés
             let data = await API.getEmployees();
             console.log(data);
-            // Filtrage des données en fonction du texte de recherche
-            setFilteredData(data.filter(item =>
+            let searchedData = data.filter(item =>
                 item.firstName.toLowerCase().includes(filterText.toLowerCase()) ||
                 item.lastName.toLowerCase().includes(filterText.toLowerCase()) ||
                 item.startDate.toLowerCase().includes(filterText.toLowerCase()) ||
@@ -46,12 +54,17 @@ function List() {
                 item.city.toLowerCase().includes(filterText.toLowerCase()) ||
                 item.state.toLowerCase().includes(filterText.toLowerCase()) ||
                 item.zipCode.toString().toLowerCase().includes(filterText.toLowerCase())
-            ));
+            );
+            // conserve les données filtrée mais pas trié pour pouvoir revenir au tri par défaut
+            setFilteredUnsortedData(searchedData);
+            // Filtrage des données en fonction du texte de recherche
+            setFilteredData(searchedData);
             // Calcul du nombre total de pages
-            setTotalPages(Math.ceil(data.length / rowsPerPage));
+            let nbpage = Math.ceil(searchedData.length / rowsPerPage);
+            setTotalPages(nbpage > 0 ? nbpage : 1);
         }
         getEmployees();
-    }, [filterText,rowsPerPage]); // Exécuté à chaque changement de filterText
+    }, [filterText, rowsPerPage]); // Exécuté à chaque changement de filterText
 
     // Gestion du changement du nombre de lignes par page
     const handlePerPageChange = (event) => {
@@ -63,6 +76,66 @@ function List() {
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
+
+    const handleSortColumns = (e) => {
+        //récupère les données filtrées mais non trié
+        let data = [...filteredUnsortedData];
+        // récupère le texte qui est dans le th qui vient d'être cliqué
+        let column = e.target.textContent
+        // récupère le sens de tri de la colonne en cherchant dans le tableau de sens le nom de la colonne (trouvée au dessus) (XX)
+        let sens = sortColumn.find(el => el.columnName === column).sort;
+        // calcule le nouveau sens. le sens est une image !!!
+        // si sens était l'image both (deux fleches) alors le nouveau sens est l'image flêche en haut
+        // si sens était l'image asc (fleche haut) alors le nouveau sens est l'image flêche en bas 
+        // si sens était l'image desc (flechebas) alors le nouveau sens est l'image deux fleches
+        sens = sens === bothImage ? ascImage : sens === ascImage ? descImage : bothImage;
+        // on met dans le tableau des sens des colonnes, toutes les colonnes à deux fleches sauf la colonne clique qui prend l'image contenu dans sens
+        let newSortColumn = defaultSort.map(el =>  { 
+            if(el.columnName === column) 
+                return { "columnName": el.columnName, "sort": sens } 
+            else 
+                return { "columnName": el.columnName, "sort": bothImage }
+        });
+        // mise à jour via useState, ce qui declanche la mise à jour de l'image sur la colonne
+        setSortColumn(newSortColumn);
+        // pour debugger et savoir le sens de la colonne car les valeurs des images c'est pas pratique 
+        let asc = sens === ascImage;
+        let dsc = sens === descImage;
+        let both = sens === bothImage;
+        // si on doit trier (asc ou dsc)
+        if (sens !== bothImage) {
+            // tri asc
+            data.sort((a, b) => {
+                if (column === 'First name') {
+                    return a.firstName.localeCompare(b.firstName)
+                } else if (column === 'First name') {
+                    return b.firstName.localeCompare(a.firstName)
+                } else if (column === 'Last name') {
+                    return a.lastName.localeCompare(b.lastName)
+                } else if (column === 'Start date') {
+                    return new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
+                } else if (column === 'Department') {
+                    return a.department.localeCompare(b.department)
+                } else if (column === 'Date of birth') {
+                    return new Date(a.dateOfBirth).getTime() - new Date(b.dateOfBirth).getTime()
+                } else if (column === 'Street') {
+                    return a.street.localeCompare(b.street)
+                } else if (column === 'City') {
+                    return a.city.localeCompare(b.city)
+                } else if (column === 'State') {
+                    return a.state.localeCompare(b.state)
+                } else {
+                    return a.zipCode - b.zipCode
+                }
+            });
+            // si en fait c'était desc, on inverse le tableau
+            if (sens === descImage) 
+                data.reverse();
+        }
+        // sinon on a juste les données filtrées mais non triées
+        // ici on met à jour les données à afficher pour qu'il réaffiche le tableau trié par la colonne
+        setFilteredData(data);
+    }
 
     return (
         <div className={styles.content}>
@@ -114,7 +187,7 @@ function List() {
                             {/* Boucle à travers les colonnes définies et génère un en-tête de tableau pour chacune */}
                             {columns.map((column, index) => (
                                 // Utilise l'index comme clé unique pour chaque en-tête
-                                <th key={index} className={styles.tableHeader}>{column}</th>
+                                <th key={index} className={styles.tableHeader} onClick={handleSortColumns} style={{ backgroundImage: `url(${/*XX*/sortColumn.find(el => el.columnName === column).sort})` }}>{column}</th>
                             ))}
                         </tr>
                     </thead>
